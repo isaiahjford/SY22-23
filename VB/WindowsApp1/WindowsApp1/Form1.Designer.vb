9be503a47dc1be57055a5a7ccf004175d662bb39
Isaiah <ijfor984@learner.dvusd.org>
2022-08-18 07:38:53 -0700

    Wednesday update

---
+++ VB/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class speedForm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.bronpicturebox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nameLabel4 = New System.Windows.Forms.Label()
+        Me.heightLabel5 = New System.Windows.Forms.Label()
+        Me.abiltyLabel6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.speed = New System.Windows.Forms.Label()
+        Me.handlesLabel7 = New System.Windows.Forms.Label()
+        Me.speedlabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        CType(Me.bronpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(50, 327)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Lebron"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'bronpicturebox
+        '
+        Me.bronpicturebox.Location = New System.Drawing.Point(65, 13)
+        Me.bronpicturebox.Name = "bronpicturebox"
+        Me.bronpicturebox.Size = New System.Drawing.Size(249, 186)
+        Me.bronpicturebox.TabIndex = 1
+        Me.bronpicturebox.TabStop = False
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Image = Global.WindowsApp1.My.Resources.Resources.download__1_
+        Me.PictureBox.Location = New System.Drawing.Point(26, 356)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(129, 74)
+        Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox.TabIndex = 2
+        Me.PictureBox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(348, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(348, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "height-"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(348, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "ability-"
+        '
+        'nameLabel4
+        '
+        Me.nameLabel4.AutoSize = True
+        Me.nameLabel4.Location = New System.Drawing.Point(415, 31)
+        Me.nameLabel4.Name = "nameLabel4"
+        Me.nameLabel4.Size = New System.Drawing.Size(0, 16)
+        Me.nameLabel4.TabIndex = 6
+        '
+        'heightLabel5
+        '
+        Me.heightLabel5.AutoSize = True
+        Me.heightLabel5.Location = New System.Drawing.Point(415, 86)
+        Me.heightLabel5.Name = "heightLabel5"
+        Me.heightLabel5.Size = New System.Drawing.Size(0, 16)
+        Me.heightLabel5.TabIndex = 7
+        '
+        'abiltyLabel6
+        '
+        Me.abiltyLabel6.AutoSize = True
+        Me.abiltyLabel6.Location = New System.Drawing.Point(414, 144)
+        Me.abiltyLabel6.Name = "abiltyLabel6"
+        Me.abiltyLabel6.Size = New System.Drawing.Size(0, 16)
+        Me.abiltyLabel6.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(345, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 16)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "speed-"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(345, 134)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Handles-"
+        '
+        'speed
+        '
+        Me.speed.AutoSize = True
+        Me.speed.Location = New System.Drawing.Point(415, 134)
+        Me.speed.Name = "speed"
+        Me.speed.Size = New System.Drawing.Size(0, 16)
+        Me.speed.TabIndex = 11
+        '
+        'handlesLabel7
+        '
+        Me.handlesLabel7.AutoSize = True
+        Me.handlesLabel7.Location = New System.Drawing.Point(432, 172)
+        Me.handlesLabel7.Name = "handlesLabel7"
+        Me.handlesLabel7.Size = New System.Drawing.Size(0, 16)
+        Me.handlesLabel7.TabIndex = 12
+        '
+        'speedlabel
+        '
+        Me.speedlabel.AutoSize = True
+        Me.speedlabel.Location = New System.Drawing.Point(412, 109)
+        Me.speedlabel.Name = "speedlabel"
+        Me.speedlabel.Size = New System.Drawing.Size(0, 16)
+        Me.speedlabel.TabIndex = 13
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp1.My.Resources.Resources.download__2_
+        Me.PictureBox1.Location = New System.Drawing.Point(509, 356)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(129, 74)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(369, 327)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Kevin Durant"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApp1.My.Resources.Resources.download__3_
+        Me.PictureBox2.Location = New System.Drawing.Point(348, 356)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(129, 74)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 17
+        Me.PictureBox2.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(532, 327)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Kyrie"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.WindowsApp1.My.Resources.Resources.download
+        Me.PictureBox3.Location = New System.Drawing.Point(185, 356)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(129, 74)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 19
+        Me.PictureBox3.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(210, 327)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "GIannis"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(682, 327)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 21
+        Me.Button5.Text = "Curry"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.WindowsApp1.My.Resources.Resources.images
+        Me.PictureBox4.Location = New System.Drawing.Point(659, 356)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(129, 74)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 20
+        Me.PictureBox4.TabStop = False
+        '
+        'speedForm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 450)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.speedlabel)
+        Me.Controls.Add(Me.handlesLabel7)
+        Me.Controls.Add(Me.speed)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.abiltyLabel6)
+        Me.Controls.Add(Me.heightLabel5)
+        Me.Controls.Add(Me.nameLabel4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Controls.Add(Me.bronpicturebox)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "speedForm1"
+        Me.Text = "NBA-Dex"
+        CType(Me.bronpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents bronpicturebox As PictureBox
+    Friend WithEvents PictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents nameLabel4 As Label
+    Friend WithEvents heightLabel5 As Label
+    Friend WithEvents abiltyLabel6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents speed As Label
+    Friend WithEvents handlesLabel7 As Label
+    Friend WithEvents speedlabel As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ VB/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class speedForm1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox.Image = bronpicturebox.Image
+        nameLabel4.Text = "lebron"
+        heightLabel5.Text = "6'9"
+        abiltyLabel6.Text = "foul"
+        speedlabel.Text = "medium"
+        handlesLabel7.Text = "7/10"
+
+    End Sub
+
+    Private Sub SpeedForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub speedForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox3.Image = bronpicturebox.Image
+        nameLabel4.Text = "Ginnis"
+        heightLabel5.Text = "6'11"
+        abiltyLabel6.Text = "Dunk"
+        speedlabel.Text = "fast"
+        handlesLabel7.Text = "5/10"
+    End Sub
+End Class
